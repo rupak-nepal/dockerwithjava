585398b958cb8415cb76303006f9528b61c0807e
rupak <nepal.rupak@gmail.com>
2019-06-04 23:08:29 +0545

    managed dependency in parent and modules

--- spring-boot-modular/spring-boot-modular-service/src/main/java/np/com/rupak/springbootmodularservice/SpringBootModularServiceApplication.java
+++ spring-boot-modular/spring-boot-modular-service/src/main/java/np/com/rupak/springbootmodularservice/SpringBootModularServiceApplication.java
@@ -3,11 +3,11 @@ package np.com.rupak.springbootmodularservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class SpringBootModularServiceApplication {
 
-//    public static void main(String[] args) {
-//        SpringApplication.run(SpringBootModularServiceApplication.class, args);
-//    }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootModularServiceApplication.class, args);
+    }
 
 }
