99fa990703aa88aeee19ab1a1287ea8dfd1999b5
rupak <nepal.rupak@gmail.com>
2019-06-10 06:04:59 +0545

    sample platform created with proper setup with mvc, and modules

--- spring-boot-modular/spring-boot-modular-ui/src/main/java/np/com/rupak/springbootmodularui/SpringBootModularUiApplication.java
+++ spring-boot-modular/spring-boot-modular-ui/src/main/java/np/com/rupak/springbootmodularui/SpringBootModularUiApplication.java
@@ -2,9 +2,16 @@ package np.com.rupak.springbootmodularui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootModularUiApplication {
+public class SpringBootModularUiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootModularUiApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootModularUiApplication.class, args);
